75306ec6311b5ad0ad5d66ccc119f9be7de515f6
anishwar00 <anishwaran27@gmail.com>
2026-03-05 21:09:06 +0530

    Create SOLUTION.cpp

---
+++ LEETCODE/Two Sum/SOLUTION.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+         
+        unordered_map<int, int> lookup;  
+        
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            
+            // Check if complement already exists
+            if (lookup.find(complement) != lookup.end()) {
+                return {lookup[complement], i};
+            }
+            
+    
+            lookup[nums[i]] = i;
+        }
+        
+        return {};
+        
+    }
+};
