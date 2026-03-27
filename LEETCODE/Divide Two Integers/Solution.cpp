081eda63912b77dc73c893131de2d4b4df8e66af
anishwar00 <anishwaran27@gmail.com>
2026-03-27 10:41:19 +0530

    Create Solution.cpp

---
+++ LEETCODE/Divide Two Integers/Solution.cpp
@@ -0,0 +1,30 @@
+#include <climits>
+#include <cmath>
+
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+
+        if (dividend == INT_MIN && divisor == -1) return INT_MAX;
+
+        bool isNegative = (dividend < 0) ^ (divisor < 0);
+
+
+        long long a = std::abs((long long)dividend);
+        long long b = std::abs((long long)divisor);
+        long long quotient = 0;
+
+        while (a >= b) {
+            long long temp = b, multiple = 1;
+
+            while (a >= (temp << 1)) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+            a -= temp;
+            quotient += multiple;
+        }
+
+        return isNegative ? -quotient : quotient;
+    }
+};
